6893bdf04957715916879ec4ef0aa45a5fa7c78c
jomatheux <jomatheux@gmail.com>
2025-04-29 14:00:31 -0300

    Ajuste no CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [`${process.env.FRONTEND_URL}`],
+    origin: [`${process.env.FRONTEND_URL}`, "https://hublocal-frontend-teste-qqlv.vercel.app"],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
